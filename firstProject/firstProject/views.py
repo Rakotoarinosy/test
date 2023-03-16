88f15b305acc66f7402ea1f2d22727fe62764e6e
Rakotoarinosy <123229108+Rakotoarinosy@users.noreply.github.com>
2023-03-17 00:57:31 +0600

    Add files via upload

---
+++ firstProject/firstProject/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'index.html')
+
+def home(request):
+    return render(request,"homepage.html")
+
+def contact(request):
+    return render(request,"contact.html")
+    
+
